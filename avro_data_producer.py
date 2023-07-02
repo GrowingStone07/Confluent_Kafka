1aec341a796e18ae58d566ca539331893f60af71
GrowingStone07 <106248741+GrowingStone07@users.noreply.github.com>
2023-07-02 10:49:25 +0530

    Update avro_data_producer.py

--- avro_data_producer.py
+++ avro_data_producer.py
@@ -30,7 +30,7 @@ def delivery_report(err, msg):
 
     """
     if err is not None:
-        print("Delivery failed for User record {}: {}".format(msg.key(), err))
+        print("Delivery failed for record {}: {}".format(msg.key(), err))
         return
     print('User record {} successfully produced to {} [{}] at offset {}'.format(
         msg.key(), msg.topic(), msg.partition(), msg.offset()))
